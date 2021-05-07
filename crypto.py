d642aae80099ac2e9b3c6febdebf82bbc7418a21
kokilamanokaran <77234372+kokilamanokaran@users.noreply.github.com>
2021-05-08 05:13:31 +0800

    Add files via upload

---
+++ crypto.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+"""
+Stanford CS106A Crypto Project
+"""
+
+import sys
+
+# provided ALPHABET constant - list of the regular alphabet
+# in lowercase. Refer to this simply as ALPHABET in your code.
+# This list should not be modified.
+ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def compute_slug(key):
+    """
+    Given a key string, compute and return the len-26 slug list for it.
+    >>> compute_slug('z')
+    ['z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y']
+    >>> compute_slug('Bananas!')
+    ['b', 'a', 'n', 's', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    >>> compute_slug('Life, Liberty, and')
+    ['l', 'i', 'f', 'e', 'b', 'r', 't', 'y', 'a', 'n', 'd', 'c', 'g', 'h', 'j', 'k', 'm', 'o', 'p', 'q', 's', 'u', 'v', 'w', 'x', 'z']
+    >>> compute_slug('Zounds!')
+    ['z', 'o', 'u', 'n', 'd', 's', 'a', 'b', 'c', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'p', 'q', 'r', 't', 'v', 'w', 'x', 'y']
+    """
+    pass
+
+
+def encrypt_char(source, slug, char):
+    """
+    Given source and slug lists,
+    if char is in source return
+    its encrypted form. Otherwise
+    return the char unchanged.
+    >>> # Using 'z' slug for testing.
+    >>> # Can set a var within a Doctest like this.
+    >>> slug = ['z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y']
+    >>> encrypt_char(ALPHABET, slug, 'A')
+    'Z'
+    >>> encrypt_char(ALPHABET, slug, 'n')
+    'm'
+    >>> encrypt_char(ALPHABET, slug, 'd')
+    'c'
+    >>> encrypt_char(ALPHABET, slug, '.')
+    '.'
+    >>> encrypt_char(ALPHABET, slug, '\\n')
+    '\\n'
+    """
+    pass
+
+
+def encrypt_str(source, slug, s):
+    """
+    Given source and slug lists and string s,
+    return a version of s where every char
+    has been encrypted by source/slug.
+    >>> slug = ['z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y']
+    >>> encrypt_str(ALPHABET, slug, 'And like a thunderbolt he falls.\\n')
+    'Zmc khjd z sgtmcdqanks gd ezkkr.\\n'
+    """
+    pass
+
+
+def decrypt_str(source, slug, s):
+    """
+    Given source and slug lists, and encrypted string s,
+    return the decrypted form of s.
+    >>> slug = ['z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y']
+    >>> decrypt_str(ALPHABET, slug, 'Zmc khjd z sgtmcdqanks gd ezkkr.\\n')
+    'And like a thunderbolt he falls.\\n'
+    """
+    pass
+
+
+def encrypt_file(filename, key):
+    """
+    Given filename and key, compute and
+    print the encrypted form of its lines.
+    """
+    pass
+
+
+def decrypt_file(filename, key):
+    """
+    Given filename and key, compute and
+    print the decrypted form of its lines.
+    """
+    pass
+
+
+def main():
+    args = sys.argv[1:]
+    # 2 command line argument patterns:
+    # -encrypt key filename
+    # -decrypt key filename
+    # Call encrypt_file() or decrypt_file() based on the args.
+    pass
+
+
+# Python boilerplate.
+if __name__ == '__main__':
+    main()
